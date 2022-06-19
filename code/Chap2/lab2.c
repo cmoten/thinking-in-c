6c7528659651471ba1a8f60cb9fc795f6612c2d5
cmoten <cmoten@gmail.com>
2022-06-19 18:13:21 +0200

    Completed chapter 2

---
+++ code/Chap2/lab2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    float x;
+    int n;
+
+    printf("Enter a real number: ");
+    fflush(stdout);
+    scanf("%f", &x);
+    n = (int)(x + 0.5);
+    printf("%f rounded == %d\n", x, n);
+    return 0;
+}
--- code/Chap2/missing.c
+++ code/Chap2/missing.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <limits.h>
 
-main() {
-    float x = ULONG_MAX;    /* 4,294,967,295 */
+int main() {
+    float x = ULONG_MAX;    /* 18,446,744,073,709,551,616 */
     double y = ULONG_MAX;
     long double z = ULONG_MAX;
 
     printf("%f\n%f\n%Lf\n",x,y,z);
+
+    return 0;
 }
 
